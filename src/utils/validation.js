d013bb2a1ecab42adc765b88e8cd15f0aa95b21f
SilvaInac <64492568+SilvaInac@users.noreply.github.com>
2022-11-30 02:52:03 -0300

    add delete

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -12,5 +12,14 @@ const Validation = {
         }
         return validation;
     }
+    delete(data) {
+        const validation = validate.validate(data, Constraints.create);
+        if (validation) {
+            const response = Constants.ErrorValidation;
+            response.message = validation;
+            return response;
+        }
+        return validation;
+    }
 };
 module.exports = Validation;
